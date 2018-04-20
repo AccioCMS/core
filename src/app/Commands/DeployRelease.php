747fe7ee1285e8d52ec572dbd6d979162a6caba8
sopa <faton.sopa@manaferra.com>
2018-04-20 13:24:51 +0200

    deploy fix permisions first

--- src/app/Commands/DeployRelease.php
+++ src/app/Commands/DeployRelease.php
@@ -38,9 +38,9 @@ class DeployRelease extends Command
      */
     public function handle()
     {
+        $this->call('deploy:permissions');
         $this->call('deploy:clean');
         $this->call('deploy:uploads');
-        $this->call('deploy:permissions');
         $this->call('deploy:env');
         $this->call('deploy:db');
     }
