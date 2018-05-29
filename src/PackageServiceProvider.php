db97170af2d01b471d9d23cc1905cb5047babefc
sopa <faton.sopa@manaferra.com>
2018-05-29 15:00:37 +0200

    dd command added

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -33,6 +33,7 @@ use Accio\App\Commands\MakeTheme;
 use Accio\App\Commands\MakeUser;
 use Illuminate\Config\Repository as ConfigRepository;
 use Accio\App\Commands\PluginInstall;
+use Spatie\ArtisanDd\DdCommand;
 
 class PackageServiceProvider extends ServiceProvider{
 
@@ -75,7 +76,8 @@ class PackageServiceProvider extends ServiceProvider{
         ActivateNewReleaseAfter::class,
         ActivateNewReleaseBefore::class,
         PurgeOldReleaseBefore::class,
-        PurgeOldReleaseAfter::class
+        PurgeOldReleaseAfter::class,
+        DdCommand::class
     ];
 
     /**
