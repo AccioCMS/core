0ad885b2bb6b766076c966aa4a7731d742a00556
sopa <faton.sopa@manaferra.com>
2018-06-11 17:45:26 +0200

    paginate added on collection

--- src/support/PostCollection.php
+++ src/support/PostCollection.php
@@ -2,6 +2,8 @@
 namespace Accio\Support;
 
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
 
 class PostCollection extends Collection
 {
@@ -22,6 +24,31 @@ class PostCollection extends Collection
      */
     public function unpublished(){
         return $this->where('published_at', '>', date('Y-m-d H:i:s'));
+    }
+
+    /**
+     * Create a pagination of items from array or collection.
+     *
+     * @param array|Collection      $items
+     * @param int  $perPage
+     * @param int  $page
+     * @param array $options
+     *
+     * @return LengthAwarePaginator
+     */
+    public function paginate($perPage = 15, $page = null, $options = [], $setPath = null)
+    {
+        $page = $page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = ($this instanceof Collection || $this instanceof PostCollection)? $this : Collection::make($items);
+        $paginator = new LengthAwarePaginator($items->forPage($page, $perPage), $items->count(), $perPage, $page, $options);
+
+        // Set Path
+        if(is_null($setPath)) {
+            $paginator->setPath(\Request::url());
+        }else{
+            $paginator->setPath($setPath);
+        }
+        return $paginator;
     }
 
 }
