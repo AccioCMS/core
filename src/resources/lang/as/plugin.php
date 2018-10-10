5ddbb012e042d9eb2d1cd116820fe765ab1e9254
Jetmir Haxhisefa <jettmirh@gmail.com>
2018-10-10 11:46:01 +0200

    Langs

---
+++ src/resources/lang/as/plugin.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'appName' => "Plugins",
+    'label' => "Plugins",
+];
---
+++ src/resources/lang/as/postType.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    'title' => 'Post type',
+    'label' => 'Post types',
+    'titlePlural' => 'Post types',
+    'listTitle' => 'All types',
+    'updateTitle' => 'Update post type',
+    'add' => 'Add new',
+    'update' => 'update',
+    'sureToDeleteField' => 'Are you sure to delete this field ?',
+    'deleteFieldWarning' => 'All data of this field on the post table will be erased',
+
+    'listTableTitle' => 'List of post types',
+    'listTableColumns' => array(
+        'id' => 'ID',
+        'name' => 'Name',
+        'slug' => 'Slug',
+        'action' => 'Action',
+    ),
+
+    'createFormTitle' => 'Create post type form',
+    'updateFormTitle' => 'Update post type form',
+    'fieldsTitle' => 'Fields',
+    'form' => array(
+        'name' => 'Post type name',
+        'hasCategories' => 'Has categories',
+        'isCategoryRequired' => 'Is category required',
+        'hasTags' => 'Has tags',
+        'isTagRequired' => 'Is tag required',
+        'hasFeaturedImage' => 'Has Featured image',
+        'isFeaturedImageRequired' => 'Is Featured image required',
+        'hasFeaturedVideo' => 'Has Featured video',
+        'isFeaturedVideoRequired' => 'Is featured video required',
+    ),
+
+];
